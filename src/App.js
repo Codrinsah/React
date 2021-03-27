e085d89f4e800dd1244157dd57fdb3ccfcd1fb71
codrin <cc1619@ic.ac.uk>
2021-03-27 21:32:36 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
+import { cleanup } from '@testing-library/react';
 import {BrowserRouter, Link, Switch, Route} from 'react-router-dom'
+import 'bulma'
 
 import {HomePage} from './pages/Home';
 import {LivePricePage} from './pages/LivePrice';
 
 function App() {
-
   return (
-  <BrowserRouter>
-    <div>
-      <div>
-        <ul>
-          <li> <Link to = "/"> Home </Link></li>
-          <li> <Link to = "/pricing"> Live Pricing </Link></li>
-        </ul>
+  <div>
+    <h1 class = "is-size-1"> 
+      Welcome to *Ready Recipies*</h1>
+    <h2 class = "is-size-2"> 
+      Please select the desired meal type and ingredients from below 
+    </h2>
+    <br/>
+    <div class = "columns">
+      <div class = "column has-background-danger-light">
+        1<br/>
+        <label class="checkbox">
+          <input type="checkbox"/> Remember me
+        </label>
       </div>
-      <Switch>
-        <Route path = "/pricing">
-          <LivePricePage />
-        </Route>
-        <Route path = "/">
-          <HomePage />
-        </Route>
-      </Switch>
+      <div class = "column has-background-danger-light">
+        2
+      </div>
+      <div class = "column has-background-danger-light">
+        3
+      </div>
     </div>
-  </BrowserRouter>
-  );
+  </div>
+  )
 }
 
 export default App;
